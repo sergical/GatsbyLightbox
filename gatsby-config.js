a7526265664a47c5fe408079ab6b2ba948c189ad
416serg <hey@416serg.me>
2018-10-28 19:02:11 -0400

    updated naming

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Lightbox Example',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -25,8 +25,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'gatsby-lightbox-example',
+        short_name: 'lightbox-example',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
